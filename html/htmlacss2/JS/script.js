d7e43c12622349bfdd5b351e5eedb325c609b136
Zestak <self.improvement1227@gmail.com>
2025-01-08 21:18:59 +0100

    FIXED ITTT

--- html/htmlacss2/JS/script.js
+++ html/htmlacss2/JS/script.js
@@ -2,6 +2,7 @@
     $(function(){
         $('a[href*="#our-pizza"]').on('click', function(e){
             e.preventDefault();
+            console.log("Scroll event triggered");
             $('html, body').animate({
                 scrollTop: $($(this).attr('href')).offset().top
             }, 250, 'linear');
@@ -52,9 +53,75 @@
         });
     });
 })(jQuery);
+
+(function($){
+     $(function(){
+        $('a[href*="#top"]').on('click', function(e){
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 400, 'linear');
+})
+
+
+
+
+
+
+
+
 $(".jq--nav-icon").click(function(){
-    console.log("Click event triggered");
-    $(".nav-background").fadeToggle(200);
+    $(".nav-background, .mobile-nav-back, nav").toggleClass("js-nav-open");
+    console.log("Nav toggle clicked");
+
   });
-(jQuery);
+
+        // $(".jq--nav-icon").click(function(){
+            
+        //     $(".nav-background ").fadeToggle(200);
+        //     $(".mobile-nav-back .js-nav-open").slideToggle(200);
+        //     $("nav ul .js-nav-open").slideToggle(200);
+          
+        //   });
+
+  
+        //   $(".jq--nav-icon").click(function(){
+            
+        //     $(".nav-background ").fadeToggle(200);
+        //     $(".mobile-nav-back ").slideToggle(200);
+        //     $("nav ul").slideToggle(200);
+          
+        //   });
+
+  
+
+
+
+
+
+
+
+
+
+
+$(".jq--nav-icon").click(function(){
+    var src = $(".jq--image-hamburger").attr("src");
+    if(src !== undefined && src.endsWith("hamburger.svg")) {
+        $(".jq--image-hamburger").attr("src","img/xmark-solid.svg");
+        console.log("switch1");
+    } else {
+        $(".jq--image-hamburger").attr("src","img/hamburger.svg");
+        console.log("switch2");
+    }
+});
+
+
+
+
+
+
+
+
+      });
+  })(jQuery);
 
